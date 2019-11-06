e6daf0bb0ddee4dc2987629fa1a509caacb66194
SergiMC <sergimuca59@gmail.com>
2019-11-06 16:18:12 +0100

    Update install.sh

--- samba:20/install.sh
+++ samba:20/install.sh
@@ -15,9 +15,7 @@ echo "local03" | passwd --stdin local03
 cp /opt/docker/nslcd.conf /etc/nslcd.conf
 cp /opt/docker/ldap.conf /etc/openldap/ldap.conf
 cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
-cp /opt/docker/system-auth-edt /etc/pam.d/system-auth-edt
-cp /opt/docker/pam_mount.conf.xml /etc/security/pam_mount.conf.xml
-ln -sf /etc/pam.d/system-auth-edt /etc/pam.d/system-auth
+
 
 /usr/sbin/nslcd && echo "nslcd Ok"
 /usr/sbin/nscd && echo "nscd Ok"
