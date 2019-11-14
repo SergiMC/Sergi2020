ea03ece29d73f2be36c01ee7e4f42f70f6b7bbbe
SergiMC <sergimuca59@gmail.com>
2019-11-14 12:20:11 +0100

    Update install.sh

--- samba:20/install.sh
+++ samba:20/install.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
-# @edt ASIX M06 2018-2019
+# @edt ASIX Sergi Muñoz Carmona 2019-2020
 # instal.lacio
 # -------------------------------------
 groupadd localgrp01
-groupadd localgrp02
 useradd -g users -G localgrp01 local01
 useradd -g users -G localgrp01 local02
 useradd -g users -G localgrp01 local03
@@ -11,16 +10,15 @@ echo "local01" | passwd --stdin local01
 echo "local02" | passwd --stdin local02
 echo "local03" | passwd --stdin local03
 
-#bash /opt/docker/auth.sh
 cp /opt/docker/nslcd.conf /etc/nslcd.conf
 cp /opt/docker/ldap.conf /etc/openldap/ldap.conf
 cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
 
-
 /usr/sbin/nslcd && echo "nslcd Ok"
 /usr/sbin/nscd && echo "nscd Ok"
 
-# -----------------------------------------------------------
+# --------- Creació dels homes dels usuaris LDAP-------------#
+
 mkdir /tmp/home
 mkdir /tmp/home/pere
 mkdir /tmp/home/pau
@@ -29,12 +27,16 @@ mkdir /tmp/home/marta
 mkdir /tmp/home/jordi
 mkdir /tmp/home/admin
 
+#--------------- Xixa als homes dels usuaris ----------------#
+
 cp README.md /tmp/home/pere
 cp README.md /tmp/home/pau
 cp README.md /tmp/home/anna
 cp README.md /tmp/home/marta
 cp README.md /tmp/home/jordi
 cp README.md /tmp/home/admin
+
+#Apliquem els permissos i associem els homes als usuaris i grups#
 
 chown -R pere.users /tmp/home/pere
 chown -R pau.users /tmp/home/pau
@@ -52,6 +54,8 @@ mkdir /var/lib/samba/privat
 #chmod 777 /var/lib/samba/privat
 cp /opt/docker/smb.conf /etc/samba/smb.conf
 cp /opt/docker/*.md /var/lib/samba/privat/.
+
+#--- Creem els usuaris smb amb autenticació de kerberos ---#
 
 echo -e "kpere\nkpere" | smbpasswd -a pere
 echo -e "kpau\nkpau" | smbpasswd -a pau
