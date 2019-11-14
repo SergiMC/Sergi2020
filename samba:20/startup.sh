1665cfc8adadf636da06a74b2fb817d93d95af93
SergiMC <sergimuca59@gmail.com>
2019-11-14 12:22:31 +0100

    Update startup.sh

--- samba:20/startup.sh
+++ samba:20/startup.sh
@@ -1,12 +1,9 @@
 #! /bin/bash
-# @edt ASIX M06 2018-2019
+# @edt ASIX Sergi MUñoz Carmona 2019-2020
 # startup.sh
 # -------------------------------------
 
 /opt/docker/install.sh && echo "Install Ok"
-
-#/usr/sbin/nslcd && echo "nslcd Ok"
-#/usr/sbin/nscd && echo "nscd Ok"
 /usr/sbin/smbd && echo "smb Ok"
 /usr/sbin/nmbd && echo "nmb  Ok"
 
