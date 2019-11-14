881fbb1d0e70ec51daa29da3b13f87b35a4bc53e
SergiMC <sergimuca59@gmail.com>
2019-11-14 12:27:00 +0100

    Update startup.sh

--- kserver:20/startup.sh
+++ kserver:20/startup.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+# @edt Sergi Muñoz Carmona 2019-2020
+# Startup
+# -------------------------------------
 /opt/docker/install.sh && echo "Ok install"
 /usr/sbin/krb5kdc
 /usr/sbin/kadmind -nofork 
