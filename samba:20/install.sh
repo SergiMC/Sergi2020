7fb4a95e5399001e0bf5d06bb6da46a1a00757db
SergiMC <sergimuca59@gmail.com>
2019-11-14 12:22:52 +0100

    Update install.sh

--- samba:20/install.sh
+++ samba:20/install.sh
@@ -51,7 +51,6 @@ chmod 777 /var/lib/samba/public
 cp /opt/docker/* /var/lib/samba/public/.
 
 mkdir /var/lib/samba/privat
-#chmod 777 /var/lib/samba/privat
 cp /opt/docker/smb.conf /etc/samba/smb.conf
 cp /opt/docker/*.md /var/lib/samba/privat/.
 
