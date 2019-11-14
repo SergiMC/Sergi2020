f6f04d223144df223ec60eb78f3e9559f73d49c9
SergiMC <sergimuca59@gmail.com>
2019-11-14 12:31:17 +0100

    Update startup.sh

--- kclient:20/startup.sh
+++ kclient:20/startup.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+# @edt Sergi Muñoz Carmona 2019-2020
+# startup
+# -------------------------------------
 /opt/docker/install.sh && echo "Ok install"
 /usr/sbin/nslcd && echo "nslcd Ok"
 /usr/sbin/nscd && echo "nscd Ok"
