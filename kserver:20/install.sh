a97d11341b3fcf413775ee999ba8c765508e9431
SergiMC <sergimuca59@gmail.com>
2019-11-14 12:26:32 +0100

    Update install.sh

--- kserver:20/install.sh
+++ kserver:20/install.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
+# @edt Sergi Muñoz Carmona 2019-2020
+# install
+# -------------------------------------
+
+#---------- Fitxers de configuració --------------#
 
 cp /opt/docker/krb5.conf /etc/krb5.conf
 cp /opt/docker/kdc.conf /var/kerberos/krb5kdc/kdc.conf
 cp /opt/docker/kadm5.acl /var/kerberos/krb5kdc/kadm5.acl
+
+#--- Creació de la base de dades i principals ---#
 
 kdb5_util create -s -P masterkey
 kadmin.local -q "addprinc -pw kpere pere/admin"
