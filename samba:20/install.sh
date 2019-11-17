917dfaade55e425b29a1030d26296c920a10ba87
SergiMC <sergimuca59@gmail.com>
2019-11-17 19:06:19 +0100

    Update install.sh

--- samba:20/install.sh
+++ samba:20/install.sh
@@ -22,28 +22,21 @@ cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
 mkdir /tmp/home
 mkdir /tmp/home/pere
 mkdir /tmp/home/pau
-mkdir /tmp/home/anna
-mkdir /tmp/home/marta
 mkdir /tmp/home/jordi
-mkdir /tmp/home/admin
+
 
 #--------------- Xixa als homes dels usuaris ----------------#
 
 cp README.md /tmp/home/pere
 cp README.md /tmp/home/pau
-cp README.md /tmp/home/anna
-cp README.md /tmp/home/marta
 cp README.md /tmp/home/jordi
-cp README.md /tmp/home/admin
 
 #Apliquem els permissos i associem els homes als usuaris i grups#
 
 chown -R pere.users /tmp/home/pere
 chown -R pau.users /tmp/home/pau
-chown -R anna.alumnes /tmp/home/anna
-chown -R marta.alumnes /tmp/home/marta
 chown -R jordi.users /tmp/home/jordi
-chown -R admin.wheel /tmp/home/admin
+
 
 # -----------------------------------------------------------
 mkdir /var/lib/samba/public
@@ -58,8 +51,6 @@ cp /opt/docker/*.md /var/lib/samba/privat/.
 
 echo -e "kpere\nkpere" | smbpasswd -a pere
 echo -e "kpau\nkpau" | smbpasswd -a pau
-echo -e "kanna\nkanna" | smbpasswd -a anna
-echo -e "kmarta\nkmarta" | smbpasswd -a marta
 echo -e "kjordi\nkjordi" | smbpasswd -a jordi
-echo -e "kadmin\nkadmin" | smbpasswd -a admin
+
 echo "Ok users"
