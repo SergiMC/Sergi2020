73e37edf651e41a6beb68c6711b06f85300e1907
SergiMC <sergimuca59@gmail.com>
2019-11-17 18:57:23 +0100

    Update install.sh

--- nfsserver:20/install.sh
+++ nfsserver:20/install.sh
@@ -10,11 +10,36 @@ echo "local01" | passwd --stdin local01
 echo "local02" | passwd --stdin local02
 echo "local03" | passwd --stdin local03
 
+# --------- Creació dels homes dels usuaris LDAP-------------#
+
+mkdir /tmp/home
+mkdir /tmp/home/pere
+mkdir /tmp/home/pau
+mkdir /tmp/home/anna
+mkdir /tmp/home/marta
+mkdir /tmp/home/jordi
+mkdir /tmp/home/admin
+
+#--------------- Xixa als homes dels usuaris ----------------#
+
+cp README.md /tmp/home/pere
+cp README.md /tmp/home/pau
+cp README.md /tmp/home/anna
+cp README.md /tmp/home/marta
+cp README.md /tmp/home/jordi
+cp README.md /tmp/home/admin
+
+#Apliquem els permissos i associem els homes als usuaris i grups#
+
+chown -R pere.users /tmp/home/pere
+chown -R pau.users /tmp/home/pau
+chown -R anna.alumnes /tmp/home/anna
+chown -R marta.alumnes /tmp/home/marta
+chown -R jordi.users /tmp/home/jordi
+chown -R admin.wheel /tmp/home/admin
+
 bash /opt/docker/auth.sh
 cp /opt/docker/nslcd.conf /etc/nslcd.conf
 cp /opt/docker/ldap.conf /etc/openldap/ldap.conf
 cp /opt/docker/nsswitch.conf /etc/nsswitch.conf
-cp /opt/docker/system-auth-edt /etc/pam.d/system-auth-edt
-cp /opt/docker/pam_mount.conf.xml /etc/security/pam_mount.conf.xml
-ln -sf /etc/pam.d/system-auth-edt /etc/pam.d/system-auth
 
