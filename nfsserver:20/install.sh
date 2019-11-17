689974ac08fa657885578323a77298961666327b
SergiMC <sergimuca59@gmail.com>
2019-11-17 18:18:17 +0100

    Update install.sh

--- nfsserver:20/install.sh
+++ nfsserver:20/install.sh
@@ -4,19 +4,12 @@
 #  - crear usuaris locals
 # -------------------------------------
 groupadd localgrp01
-groupadd localgrp02
 useradd -g users -G localgrp01 local01
 useradd -g users -G localgrp01 local02
 useradd -g users -G localgrp01 local03
-useradd -g users -G localgrp02 local04
-useradd -g users -G localgrp02 local05
-useradd -g users -G localgrp02 local06
 echo "local01" | passwd --stdin local01
 echo "local02" | passwd --stdin local02
 echo "local03" | passwd --stdin local03
-echo "local04" | passwd --stdin local04
-echo "local05" | passwd --stdin local05
-echo "local06" | passwd --stdin local06
 
 bash /opt/docker/auth.sh
 cp /opt/docker/nslcd.conf /etc/nslcd.conf
