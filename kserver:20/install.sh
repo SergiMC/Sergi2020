c046d61ba0b0a281c27af391b46443335585ea09
SergiMC <sergimuca59@gmail.com>
2019-11-18 14:02:11 +0100

    Update install.sh

--- kserver:20/install.sh
+++ kserver:20/install.sh
@@ -12,12 +12,12 @@ cp /opt/docker/kadm5.acl /var/kerberos/krb5kdc/kadm5.acl
 #--- Creació de la base de dades i principals ---#
 
 kdb5_util create -s -P masterkey
+kadmin.local -q "addprinc -pw ksergi sergi"
 kadmin.local -q "addprinc -pw kpere pere/admin"
 kadmin.local -q "addprinc -pw kanna anna/admin"
 kadmin.local -q "addprinc -pw kpau pau"
 kadmin.local -q "addprinc -pw kjordi jordi"
 kadmin.local -q "addprinc -pw kmarta marta"
-kadmin.local -q "addprinc -pw kjulia julia"
 
 
 
