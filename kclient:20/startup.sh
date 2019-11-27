f9c5468643efe1b4d846bcab4685f1b6d72dec05
SergiMC <sergimuca59@gmail.com>
2019-11-27 13:25:33 +0100

    Update startup.sh

--- kclient:20/startup.sh
+++ kclient:20/startup.sh
@@ -5,5 +5,10 @@
 /opt/docker/install.sh && echo "Ok install"
 /usr/sbin/nslcd && echo "nslcd Ok"
 /usr/sbin/nscd && echo "nscd Ok"
+/usr/sbin/rpcbind && echo "rpcbind Ok"
+/usr/sbin/rpc.statd && echo "rpc.stad Ok"
+/usr/sbin/rpc.nfsd && echo "rpc.nfsd Ok"
+/usr/sbin/rpc.mountd && echo "rpc.mountd Ok"
+/usr/sbin/exportfs -av
 /bin/bash
 
